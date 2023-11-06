7e72ad01dfed5fe2a8a465fbc5f4dc1ea725115d
gimz98 <udnamod98@gmail.com>
2023-11-06 22:17:04 +0530

    Update

--- functions/newNote.cjs
+++ functions/newNote.cjs
@@ -29,7 +29,7 @@ exports.handler = async (event, context)=>{
         catch(err){
             return{
                 statusCode: 200,
-                body: JSON.stringify({Error: err})
+                body: JSON.stringify({Body of the request: body})
             } 
         }
 
