ba58be1475aa7f54ac9bf5fdf5e499f95ec8b992
gimz98 <udnamod98@gmail.com>
2023-11-06 21:18:49 +0530

    userNotes functionality complete

--- functions/userNotes.cjs
+++ functions/userNotes.cjs
@@ -1,24 +1,43 @@
 var Notedb = require('./models/note.cjs');
+const { connect, close } = require('./database/connection.cjs')
 
-exports.handler = (req,res)=>{
-    if(!req.body){
-        return res.status(400).send({message:"Data to update can not be empty"});
+exports.handler = async (event, context)=>{
+    try{
+        await connect()
+        var { httpMethod, path, body, queryStringParameters} = event;
+
+        if (httpMethod != 'GET'){
+            return{
+                statusCode: 500,
+                body: JSON.stringify({message: "Wrong method"})
+            }  
+        }
+    
+        if(queryStringParameters["id"]){
+            const id = queryStringParameters["id"];
+    
+            const data = await Notedb.find({ userId:id });
+            if(!data){
+                return{
+                    statusCode: 404,
+                    body: JSON.stringify({message: "No notes for user with id: "+ id})
+                }  
+            }
+    
+            return {
+                statusCode: 200,
+                body: JSON.stringify(data)
+            } 
+        }
+        else{
+            return{
+                statusCode: 500,
+                body: JSON.stringify({message: "Need user Id"})
+            }  
+        }
+    }
+    finally{
+        await close()
     }
 
-    const id = req.params.id;
-    
-    Notedb.find({ userId:id })
-        .then(data=>{
-            if(!data){
-                res.status(404).send({message:`Can't find notes for user with id ${id}`});
-            }
-            else{
-                res.status(200).send(data);
-
-            }
-        })
-        .catch(err=>{
-            res.status(500).send({message:"Error getting notes"});
-        })
-        
 }
