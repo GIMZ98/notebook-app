00d63ad3393a239fbc7c30668dcfc31df927d5a5
gimz98 <udnamod98@gmail.com>
2023-11-06 09:08:04 +0530

    Checking serverless functions

--- functions/controller/findNote.cjs
+++ functions/controller/findNote.cjs
@@ -1,33 +1,37 @@
 var Notedb = require('../models/note.cjs');
 
-exports.handler = (req,res)=>{
+exports.handler = (event)=>{
 
-    if(req.query.id){
-        const id = req.query.id;
+    //var { httpMethod, path, body} = event;
 
-        Notedb.findById(id)
-        .then(data=>{
-            if(!data){
-                res.status(404).send({message: "Not found note with id "+ id});
-            }
-            else{
-                res.send(data);
-                console.log("note data: ", data)
-            }
-        })
-        .catch(err=>{
-            res.status(500).send({message:"Error retrieving note with id "+ id});        
-        })
-    }
-    else{
-        Notedb.find()
-        .then(data=>{
-            res.send(data);
-            console.log("all notes: ", data)
-        })
-        .catch(err=>{
-            res.status(500).send({message:err.message});
-        })
-    }
+    // if(req.query.id){
+    //     const id = req.query.id;
+
+    //     Notedb.findById(id)
+    //     .then(data=>{
+    //         if(!data){
+    //             res.status(404).send({message: "Not found note with id "+ id});
+    //         }
+    //         else{
+    //             res.send(data);
+    //             console.log("note data: ", data)
+    //         }
+    //     })
+    //     .catch(err=>{
+    //         res.status(500).send({message:"Error retrieving note with id "+ id});        
+    //     })
+    // }
+    // else{
+    //     Notedb.find()
+    //     .then(data=>{
+    //         res.send(data);
+    //         console.log("all notes: ", data)
+    //     })
+    //     .catch(err=>{
+    //         res.status(500).send({message:err.message});
+    //     })
+    // }
+
+    res.status(200).send({message:event});
 
 }
