c0c0d7b63bbefdbf29c9ffed2ecb13dc9a8c3a7d
gimz98 <udnamod98@gmail.com>
2023-11-06 12:11:50 +0530

    Update

--- functions/controller/findNote.cjs
+++ functions/controller/findNote.cjs
@@ -8,47 +8,38 @@ exports.handler = async (event, context)=>{
     if(queryStringParameters["id"]){
         const id = queryStringParameters["id"];
 
-        await Notedb.findById(id)
-        .then(data=>{
-            if(!data){
-                return{
-                    statusCode: 404,
-                    body: JSON.stringify({message: "Not found note with id "+ id})
-                }  
-            }
-            else{
-                console.log("note data: ", data)
-                return{
-                    statusCode: 200,
-                    body: JSON.stringify(data)
-                }  
-            }
-        })
-        .catch(err=>{
+        const data = await Notedb.findById(id);
+        if(!data){
             return{
-                statusCode: 500,
-                body: JSON.stringify({message: "Error retrieving data"})
-            }        
-        })
+                statusCode: 404,
+                body: JSON.stringify({message: "Not found note with id "+ id})
+            }  
+        }
+
+        return{
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }  
         // return{
         //     statusCode: 200,
         //     body: JSON.stringify({id: id})
         // }  
     }
     else{
-        await Notedb.find()
-        .then(data=>{
-            return{
-                statusCode: 200,
-                body: JSON.stringify(data)
-            }  
-        })
-        .catch(err=>{
+        const data = Notedb.find()
+
+        if(!data){
             return{
                 statusCode: 500,
                 body: JSON.stringify({message: "Error retrieving data"})
             }   
-        })
+        }
+
+        return{
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }  
+        
     }
 
     // return{
