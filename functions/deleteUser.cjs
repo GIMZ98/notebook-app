0fe923a0090a19cb2829b4d16357c482e3152622
gimz98 <udnamod98@gmail.com>
2023-11-07 20:33:00 +0530

    deleteUser functionality, which delete user's data

--- functions/deleteUser.cjs
+++ functions/deleteUser.cjs
@@ -1,4 +1,5 @@
 var Userdb = require('./models/user.cjs');
+var Notedb = require('./models/note.cjs');
 
 //Delete a user with given user id in the request
 exports.delete = (req,res)=>{
@@ -16,4 +17,55 @@ exports.delete = (req,res)=>{
         .catch(err=>{
             res.status(500).send({message: "Could not delete User with id="+id});
         });
+}
+
+
+var Userdb = require('./models/user.cjs');
+var Notedb = require('./models/note.cjs');
+const { connect, close } = require('./database/connection.cjs')
+
+exports.handler = async (event, context)=>{
+    try{
+        await connect()
+        var { httpMethod, path, body, queryStringParameters} = event;
+
+        if (httpMethod != 'DELETE'){
+            return{
+                statusCode: 500,
+                body: JSON.stringify({message: "Wrong method"})
+            }  
+        }
+
+    
+        if(queryStringParameters["id"]){
+            const id = queryStringParameters["id"];
+            
+            try{
+                await Userdb.findByIdAndDelete(id)
+                await Notedb.deleteMany({ userId: id })
+            }
+            catch(err){
+                return{
+                    statusCode: 500,
+                    body: JSON.stringify({error:err})
+                }  
+            }
+            
+            return {
+                statusCode: 200,
+                body: JSON.stringify({success:"User data deleted!"})
+            } 
+
+        }
+        else{
+            return{
+                statusCode: 500,
+                body: JSON.stringify({error: "Error deleting user data."})
+            }   
+        }
+    }
+    finally{
+        await close()
+    }
+
 }
