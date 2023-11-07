caecb46140bbc282aaaf607a642a3db7d9cc5c26
gimz98 <udnamod98@gmail.com>
2023-11-07 13:38:33 +0530

    login functionality

--- functions/login.cjs
+++ functions/login.cjs
@@ -1,57 +1,112 @@
+// var Userdb = require('./models/user.cjs');
+// const argon2 = require('argon2');
+
+
+
+
+// exports.handler = async (event, context)=>{
+//     //validate request
+//     if(!req.body){
+//         res.status(400).send({message: "Content can not be empty!"});
+//         return;
+//     }
+// 	console.log("login api working")
+// 	const username_ = req.body.name
+// 	const password_ = req.body.password
+
+// 	var hashPassword = '';
+// 	try{	
+// 		console.log("try block working", username_)
+// 		await Userdb.findOne({ name: username_})
+// 		.then((user) => {
+// 			console.log("user got: ", user.password)
+//             hashPassword = user.password
+
+// 		})
+// 		.catch(err => {
+// 			console.log("err")
+// 		})
+		
+//         if(await verifyPassword(hashPassword, password_)){
+//             res.status(200).send({
+//                 message:`user verified`
+//             });
+//         }
+//         else{
+//             res.status(500).send({
+//                 message:`user unauthorized`
+//             });
+//         }
+// 	}
+// 	catch(err){
+// 		console.log("Error findOne",err)
+// 		res.status(500).send({
+// 		message:`Login error`
+// 		});
+// 	}		
+	
+// }
+
+
 var Userdb = require('./models/user.cjs');
 const argon2 = require('argon2');
+const { connect, close } = require('./database/connection.cjs')
 
-const verifyPassword = async(hashedPassword, password) => {
-    if (await argon2.verify(hashedPassword, password)){
-        console.log("password matches");
-        return true;
+exports.handler = async (event, context)=>{
+    try{
+        await connect()
+        var { httpMethod, body } = event;
+
+        if (httpMethod != 'POST'){
+            return{
+                statusCode: 500,
+                body: JSON.stringify({message: "Wrong method!"})
+            }  
+        }
+
+        if (body){
+            body = JSON.parse(body)
+        }
+
+        const username_ = body.name
+        const password_ = body.password
+
+        var hashPassword = '';
+
+		await Userdb.findOne({ name: username_})
+		.then((user) => {
+            hashPassword = user.password
+		})
+		.catch(err => {
+            return{
+                statusCode: 500,
+                body: JSON.stringify({error:'Not registered!'})
+            } 
+		})
+
+        if(await verifyPassword(hashPassword, password_)){
+            return{
+                statusCode: 500,
+                body: JSON.stringify({success:'user verified'})
+            } 
+        }
+        else{
+            return{
+                statusCode: 500,
+                body: JSON.stringify({error:'user unauthorized'})
+            } 
+        }
     }
-    else{
-        console.log("wrong password");
-        return false;
+    finally{
+        await close()
     }
 }
 
-
-exports.handler = async (req,res)=>{
-    //validate request
-    if(!req.body){
-        res.status(400).send({message: "Content can not be empty!"});
-        return;
+const verifyPassword = async(hashedPassword, password) => {
+    if (await argon2.verify(hashedPassword, password)){
+        return true;
     }
-	console.log("login api working")
-	const username_ = req.body.name
-	const password_ = req.body.password
-
-	var hashPassword = '';
-	try{	
-		console.log("try block working", username_)
-		await Userdb.findOne({ name: username_})
-		.then((user) => {
-			console.log("user got: ", user.password)
-            hashPassword = user.password
-
-		})
-		.catch(err => {
-			console.log("err")
-		})
-		
-        if(await verifyPassword(hashPassword, password_)){
-            res.status(200).send({
-                message:`user verified`
-            });
-        }
-        else{
-            res.status(500).send({
-                message:`user unauthorized`
-            });
-        }
-	}
-	catch(err){
-		console.log("Error findOne",err)
-		res.status(500).send({
-		message:`Login error`
-		});
-	}		
-	
+    else{
+        return false;
+    }
 }
