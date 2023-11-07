a6a1b7be0c3bd244d3a5a2c701a202f4c6d60733
gimz98 <udnamod98@gmail.com>
2023-11-07 14:58:36 +0530

    login functionality complete

--- functions/updateNote.cjs
+++ functions/updateNote.cjs
@@ -37,7 +37,7 @@ exports.handler = async (event, context)=>{
             
             return {
                 statusCode: 200,
-                body: JSON.stringify({success:data})
+                body: JSON.stringify({success:body})
             } 
 
         }
