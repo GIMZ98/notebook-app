33255d78cb3ac9b2ad7158bb9be63ab4f1438e9a
gimz98 <udnamod98@gmail.com>
2023-11-07 14:24:16 +0530

    login functionality complete

--- functions/updateNote.cjs
+++ functions/updateNote.cjs
@@ -1,32 +1,4 @@
 var Notedb = require('./models/note.cjs');
-
-exports.handler = (req,res)=>{
-    if(!req.body){
-        return res.status(400).send({message:"Data to update can not be empty"});
-    }
-
-    const id = req.params.id;
-    const title_ = req.body.title;
-    const content_ = req.body.content;
-    
-    Notedb.findByIdAndUpdate(id, {title:title_, content:content_})
-        .then(data=>{
-            if(!data){
-                res.status(404).send({message:`Cannot Update note with ${id}`});
-            }
-            else{
-                res.status(200).send(data);
-
-            }
-        })
-        .catch(err=>{
-            res.status(500).send({message:"Error Updating note information"});
-        })
-        
-}
-
-
-var Notedb = require('./models/note.cjs');
 const { connect, close } = require('./database/connection.cjs')
 
 exports.handler = async (event, context)=>{
@@ -34,7 +6,7 @@ exports.handler = async (event, context)=>{
         await connect()
         var { httpMethod, path, body, queryStringParameters} = event;
 
-        if (httpMethod != 'GET'){
+        if (httpMethod != 'PUT'){
             return{
                 statusCode: 500,
                 body: JSON.stringify({message: "Wrong method"})
