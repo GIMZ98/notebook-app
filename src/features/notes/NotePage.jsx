4c60a1c5b6eb7ff4096bc21e40b995ab9f729a6d
gimz98 <udnamod98@gmail.com>
2023-11-08 22:49:39 +0530

    UI for note listings is complete

--- src/features/notes/NotePage.jsx
+++ src/features/notes/NotePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import $ from 'jquery'
+import { AiFillEdit } from 'react-icons/ai'
+import { RiDeleteBinLine } from 'react-icons/ri'
 
 const NotePage = () => {
 
@@ -22,16 +24,56 @@ const NotePage = () => {
                 </ul>
             </nav>
 
-            <div className='w-screen h-[100px] bg-blue-100'></div>
+            <div className='w-screen h-[100px] bg-blue-0'></div>
 
-            <div id='notesDiv' className='w-screen h-[100px] bg-slate-500'>
+            {/* Notes div */}
+            <div id='notesDiv' className='flex flex-col items-center w-screen'>
+
+                {/* Single Note */}
+                <div className='flex justify-between items-center sm:w-[600px] w-full h-[50px] bg-blue-100 border-b-2 border-black'>
+                    <div className='w-full h-[50px] text-[20px] p-[10px] font-mono truncate'>
+                        This is title  This is title  This is title This is title  This is title  This is title This is title  This is title  This is title
+                    </div>
+                    <div className='flex items-center justify-between w-[120px] h-full p-[10px]'>
+                        <AiFillEdit className='text-3xl'/>
+                        <RiDeleteBinLine className='text-3xl'/>
+                    </div>
+                </div>
+                {/* End of Single Note */}
+
+                {/* Single Note */}
+                <div className='flex justify-between items-center sm:w-[600px] w-full h-[50px] bg-blue-100 border-b-2 border-black'>
+                    <div className='w-full h-[50px] text-[20px] p-[10px] font-mono truncate'>
+                        This is title  This is title  This is title This is title  This is title  This is title This is title  This is title  This is title
+                    </div>
+                    <div className='flex items-center justify-between w-[120px] h-full p-[10px]'>
+                        <AiFillEdit className='text-3xl'/>
+                        <RiDeleteBinLine className='text-3xl'/>
+                    </div>
+                </div>
+                {/* End of Single Note */}
+
+                {/* Single Note */}
+                <div className='flex justify-between items-center sm:w-[600px] w-full h-[50px] bg-blue-100 border-b-2 border-black'>
+                    <div className='w-full h-[50px] text-[20px] p-[10px] font-mono truncate'>
+                        This is title  This is title  This is title This is title  This is title  This is title This is title  This is title  This is title
+                    </div>
+                    <div className='flex items-center justify-between w-[120px] h-full p-[10px]'>
+                        <AiFillEdit className='text-3xl'/>
+                        <RiDeleteBinLine className='text-3xl'/>
+                    </div>
+                </div>
+                {/* End of Single Note */}              
 
             </div>
+            {/* End of Notes div */}
 
+
+            {/* New Note div */}
             <div id='newNoteDiv' className='absolute flex justify-center items-center w-screen h-screen bg-slate-800 bg-opacity-80 z-[-10]'>
 
                 <div className='flex flex-col justify-between items-center sm:w-[500px] w-full sm:h-[600px] h-full bg-blue-400 py-5'>
-                    <h1 className='text-3xl font-bold text-white font-mono'>Login</h1>
+                    <h1 className='text-3xl font-bold text-white font-mono'>New Note</h1>
                     <div>
                         <input id='title' type="text" className='border-[1px] border-slate-600 p-3 focus:border-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full mb-5' placeholder='Title'/>
                         <textarea id='content' placeholder="Content..." className='border-[1px] border-slate-600 p-3 focus:border-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full' rows="15"></textarea>
@@ -41,9 +83,12 @@ const NotePage = () => {
                         <button id='submitBtn' className='bg-green-600 py-2 px-8 w-full text-white font-bold text-[16px] hover:bg-green-800 disabled:opacity-80 disabled:pointer-events-none mx-5'>Save</button>
                         <button id='cancelBtn' onClick={hideNewNote} className='bg-red-600 py-2 px-5 w-full text-white font-bold text-[16px] hover:bg-red-800 disabled:opacity-80 disabled:pointer-events-none mx-5'>Cancel</button>
                     </div>
-                   
+                
                 </div>
             </div>
+            {/*End of New Note div */}
+
+
         </div>
     </>
   )
