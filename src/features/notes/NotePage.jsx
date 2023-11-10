cd77d4cc49e74ea261fa07c9ceabcbc013e4fe89
gimz98 <udnamod98@gmail.com>
2023-11-10 17:44:24 +0530

    Saving and fetching posts via UI is complete

--- src/features/notes/NotePage.jsx
+++ src/features/notes/NotePage.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import $ from 'jquery'
 import { AiFillEdit } from 'react-icons/ai'
 import { RiDeleteBinLine } from 'react-icons/ri'
+import axios from 'axios'
 
 import { useSelector } from 'react-redux'
 import { selectUser } from '../users/userSlice'
 
+
+
 const NotePage = () => {
+
+  const [newTitle, setNewTitle] = useState('')
+  const [newContent, setNewContent] = useState('')
+  const [notes, setNotes] = useState([])
+
+  const onTitleChanged = (e) => setNewTitle(e.target.value)
+  const onContentChanged = (e) => setNewContent(e.target.value)
 
   var user = useSelector(selectUser)
   console.log("user: ", user)
+  console.log("data: ", notes)
 
   const showNewNote = () => {
     $('#newNoteDiv').addClass('z-10').removeClass('z-[-10]').removeClass('hidden')
@@ -19,6 +30,46 @@ const NotePage = () => {
   const hideNewNote = () => {
     $('#newNoteDiv').addClass('z-[-10]').addClass('hidden').removeClass('z-10')
   }
+
+  const fetchNotes = async() => {
+    await axios.get(`/.netlify/functions/userNotes?id=${user.userId}`)
+    .then(response =>{
+        console.log("response: ", response)
+        setNotes(response.data)
+    })
+    .catch(err => {
+        console.log("err: ", err)
+    })
+  }
+
+  var canSave = Boolean(newTitle) && Boolean(newContent)
+
+  const saveNote = async() => {
+    $('#submitBtn').text("Saving")
+    await axios.post('/.netlify/functions/newNote',
+            {userId: user.userId, title: newTitle, content: newContent}
+        )
+        .then(response =>{
+            console.log("response", response)
+            $('#submitBtn').text("Saved")
+            fetchNotes()
+            hideNewNote()
+            $('#title').val('')
+            $('#content').val('')
+            $('#submitBtn').text("Save")
+
+        })
+        .catch(err => {
+            console.log("err: ", err)
+
+        })
+  }
+
+  useEffect(() => {
+    console.log("changed")
+    fetchNotes();
+  }, [user])
+  
 
   return (
     <>
@@ -41,20 +92,28 @@ const NotePage = () => {
             {/* Notes div */}
             <div id='notesDiv' className='flex flex-col items-center w-screen z-0'>
 
-                {/* Single Note */}
-                <div className='flex justify-between items-center sm:w-[600px] w-full h-[50px] bg-blue-100 border-b-2 border-black'>
-                    <div className='w-full h-[50px] text-[20px] p-[10px] font-mono truncate'>
-                        This is title  This is title  This is title This is title  This is title  This is title This is title  This is title  This is title
-                    </div>
-                    <div className='flex items-center justify-between w-[120px] h-full p-[10px]'>
-                        <AiFillEdit className='text-3xl'/>
-                        <RiDeleteBinLine className='text-3xl'/>
-                    </div>
-                </div>
-                {/* End of Single Note */}
+                {
+                    notes.map((note, index) => (
+                        <div key={index}>
+                            <div className='flex justify-between items-center sm:w-[600px] w-full h-[50px] bg-blue-100 border-b-2 border-black'>
+                                <div className='w-full h-[50px] text-[20px] p-[10px] font-mono truncate'>
+                                    {note.title}
+                                </div>
+                                <div className='flex items-center justify-between w-[120px] h-full p-[10px]'>
+                                    <AiFillEdit className='text-3xl'/>
+                                    <RiDeleteBinLine className='text-3xl'/>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    ))
+
+                }
+
 
                 {/* Single Note */}
-                <div className='flex justify-between items-center sm:w-[600px] w-full h-[50px] bg-blue-100 border-b-2 border-black'>
+                {/* <div className='flex justify-between items-center sm:w-[600px] w-full h-[50px] bg-blue-100 border-b-2 border-black'>
                     <div className='w-full h-[50px] text-[20px] p-[10px] font-mono truncate'>
                         This is title  This is title  This is title This is title  This is title  This is title This is title  This is title  This is title
                     </div>
@@ -62,20 +121,10 @@ const NotePage = () => {
                         <AiFillEdit className='text-3xl'/>
                         <RiDeleteBinLine className='text-3xl'/>
                     </div>
-                </div>
+                </div> */}
                 {/* End of Single Note */}
 
-                {/* Single Note */}
-                <div className='flex justify-between items-center sm:w-[600px] w-full h-[50px] bg-blue-100 border-b-2 border-black'>
-                    <div className='w-full h-[50px] text-[20px] p-[10px] font-mono truncate'>
-                        This is title  This is title  This is title This is title  This is title  This is title This is title  This is title  This is title
-                    </div>
-                    <div className='flex items-center justify-between w-[120px] h-full p-[10px]'>
-                        <AiFillEdit className='text-3xl'/>
-                        <RiDeleteBinLine className='text-3xl'/>
-                    </div>
-                </div>
-                {/* End of Single Note */}              
+             
 
             </div>
             {/* End of Notes div */}
@@ -87,12 +136,12 @@ const NotePage = () => {
                 <div className='flex flex-col justify-between items-center sm:w-[500px] w-full sm:h-[600px] h-full bg-blue-400 py-5'>
                     <h1 className='text-3xl font-bold text-white font-mono'>New Note</h1>
                     <div>
-                        <input id='title' type="text" className='border-[1px] border-slate-600 p-3 focus:border-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full mb-5' placeholder='Title'/>
-                        <textarea id='content' placeholder="Content..." className='border-[1px] border-slate-600 p-3 focus:border-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full' rows="15"></textarea>
+                        <input id='title' onChange={onTitleChanged} type="text" className='border-[1px] border-slate-600 p-3 focus:border-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full mb-5' placeholder='Title'/>
+                        <textarea id='content' onChange={onContentChanged} placeholder="Content..." className='border-[1px] border-slate-600 p-3 focus:border-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full' rows="15"></textarea>
                     </div>
 
                     <div className='flex'>
-                        <button id='submitBtn' className='bg-green-600 py-2 px-8 w-full text-white font-bold text-[16px] hover:bg-green-800 disabled:opacity-80 disabled:pointer-events-none mx-5'>Save</button>
+                        <button id='submitBtn' onClick={saveNote} disabled={!canSave} className='bg-green-600 py-2 px-8 w-full text-white font-bold text-[16px] hover:bg-green-800 disabled:opacity-80 disabled:pointer-events-none mx-5'>Save</button>
                         <button id='cancelBtn' onClick={hideNewNote} className='bg-red-600 py-2 px-5 w-full text-white font-bold text-[16px] hover:bg-red-800 disabled:opacity-80 disabled:pointer-events-none mx-5'>Cancel</button>
                     </div>
                 
